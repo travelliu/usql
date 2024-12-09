7dfc4b003772dca4988811e49a2f12a40ef669d3
Travel.Liu <traveldba@gmail.com>
2024-12-09 18:52:14 +0800

    disable aliyun-tablestore-go-sql-driver

--- drivers/ots/ots.go
+++ drivers/ots/ots.go
@@ -3,11 +3,11 @@
 // See: https://github.com/aliyun/aliyun-tablestore-go-sql-driver
 package ots
 
-import (
-	_ "github.com/aliyun/aliyun-tablestore-go-sql-driver" // DRIVER
-	"github.com/xo/usql/drivers"
-)
+// import (
+// 	_ "github.com/aliyun/aliyun-tablestore-go-sql-driver" // DRIVER
+// 	"github.com/xo/usql/drivers"
+// )
 
-func init() {
-	drivers.Register("ots", drivers.Driver{})
-}
+// func init() {
+// 	drivers.Register("ots", drivers.Driver{})
+// }
